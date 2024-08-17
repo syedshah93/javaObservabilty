15008d50732dd6822da6e519e8819df0f80981cf
syedshah93 <sshahbaaz@ualr.edu>
2024-08-17 13:11:47 -0500

    initial commit

---
+++ src/main/java/com/mcds/observabilty/ObservabiltyApplication.java
@@ -0,0 +1,13 @@
+package com.mcds.observabilty;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ObservabiltyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ObservabiltyApplication.class, args);
+    }
+
+}
