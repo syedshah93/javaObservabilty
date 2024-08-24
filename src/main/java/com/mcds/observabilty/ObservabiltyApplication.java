848011bae37613fdf60dc7a9c189d0a5cabccb74
syedshah93 <sshahbaaz@ualr.edu>
2024-08-24 12:47:23 -0500

    Test: this is for testing the pipeline

--- src/main/java/com/mcds/observabilty/ObservabiltyApplication.java
+++ src/main/java/com/mcds/observabilty/ObservabiltyApplication.java
@@ -8,6 +8,7 @@ public class ObservabiltyApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ObservabiltyApplication.class, args);
+        System.out.println("Hello World!");
     }
 
 }
