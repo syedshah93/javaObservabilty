654bdcfeaef89ba37d3ba468df49791c839ba10a
Basith5075 <65912815+Basith5075@users.noreply.github.com>
2024-09-01 16:34:02 -0500

    Jasypt Integration

--- src/main/java/com/mcds/observabilty/ObservabiltyApplication.java
+++ src/main/java/com/mcds/observabilty/ObservabiltyApplication.java
@@ -1,9 +1,11 @@
 package com.mcds.observabilty;
 
+import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableEncryptableProperties
 public class ObservabiltyApplication {
 
     public static void main(String[] args) {
